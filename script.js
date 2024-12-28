2915bf3e5ae2c6f43a19a8dd153ad9e5378ad9fd
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2024-12-28 01:00:45 -0300

    Relação de testes

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 document.querySelector("#esquerdo").addEventListener("change", () => {
   const togle = document.querySelector("#togle")
   togle.style.background = "#D9D9D9"
+  console.log("esquerdo-cinza")
 })
 
 
 document.querySelector("#direito").addEventListener("change", () => {
   const togle = document.querySelector("#togle")
   togle.style.background = "linear-gradient(#D586E0 0%, #91A1FA 98.93%)"
+  console.log("direito-colorido")
 })
